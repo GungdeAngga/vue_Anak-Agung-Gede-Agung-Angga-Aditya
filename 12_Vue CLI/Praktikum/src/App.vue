50b73d2a790e554769ba4859e45dcaff4a818163
Angga Aditya <agungangga2001@gmail.com>
2022-03-18 09:15:43 +0800

    commit vue_CLI task

---
+++ 12_Vue CLI/Praktikum/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <ol>
+      <li v-for="item in items" :key="item.name">
+        {{ item.name }}
+      </li>
+    </ol>
+    <input type="text" v-model="TaskBaru" />
+    <button @click="addItems">Tambahkan</button>
+    <p v-if="items.length >= 4">Hebat!!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      TaskBaru: "",
+      items: [],
+    };
+  },
+  methods: {
+    addItems() {
+      if (this.TaskBaru === "") {
+        alert("input kosong mohon diisi");
+      } else {
+        this.items.push({ name: this.TaskBaru });
+        this.TaskBaru = "";
+      }
+    },
+  },
+};
+</script>
